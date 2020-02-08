759512742b6785ac0592b96ebb9e17f9ff914deb
Sayed <sadatsayed92@gmail.com>
2020-02-08 11:34:12 -0800

    workshop day

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,4 +25,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send all the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`Post operations not supported on /campsite/${req.params.campsiteId}`);})
+.put((req, res) => {
+    res.write(`Updating the campsite ${req.params.campsiteId}\n`)
+    res.end(`Will Update the campsite: ${req.body.name} with ${req.body.description}`);})
+.delete((req, res) => {
+    res.end(`Deleting campsite ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
